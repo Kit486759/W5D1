405b22662284fb189b4750551da7cad40c488e17
kit486759 <kit486759@gmail.com>
2021-03-16 09:31:37 -0700

    fix gen upper lower case and symbol

--- strings/strings.js
+++ strings/strings.js
@@ -4,71 +4,81 @@
 // const stringLib = () => 
 //     {
 
-function concatanate(a,b)
-{
-  let result = a.concat(b)
-  return result;
+function concatanate(a, b) {
+    let result = a.concat(b)
+    return result;
 }
-console.log(`concatanate a(HIHI) with b(How are you?) = ${concatanate("HIHI, ","How are you?")}`)
+console.log(`concatanate a(HIHI) with b(How are you?) = ${concatanate("HIHI, ", "How are you?")}`)
 
 
-function ASCIINumberToChar(Number)
-{
-// take one number, say, 127 and convert it to its ASCII char representation
-let result = String.fromCharCode(Number)
-return result;
+function ASCIINumberToChar(Number) {
+    // take one number, say, 127 and convert it to its ASCII char representation
+    let result = String.fromCharCode(Number)
+    return result;
 }
 console.log(`Take the number 65 and covert it to ASCII char = ${ASCIINumberToChar(65)}`)
 
-function CharToAsciiNumber(char)
-{
-// take one Character, say, 'A' and convert it to it's Number representation
-let result = char.charCodeAt(0);
-return result;
+function CharToAsciiNumber(char) {
+    // take one Character, say, 'A' and convert it to it's Number representation
+    let result = char.charCodeAt(0);
+    return result;
 }
 console.log(`Take the character "A" and covert it to number = ${CharToAsciiNumber("A")}`)
 
-function ReverseString(origionalString)
-{
-// split to array
-let arr = origionalString.split("")
-// reverse array
-let reArr = arr.reverse()
-// convert back to string
-let result = reArr.join("")
-return result;
-// return a string, ie: Hello becomes olleH
+function ReverseString(origionalString) {
+    // split to array
+    let arr = origionalString.split("")
+    // reverse array
+    let reArr = arr.reverse()
+    // convert back to string
+    let result = reArr.join("")
+    return result;
+    // return a string, ie: Hello becomes olleH
 }
 console.log(`Reverse the string "Hello" to ${ReverseString(`"Hello"`)}`)
 
+// let abc="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+// let ABC = abc.toUpperCase()
+// let array = ABC.split(',')
+// console.log(array.length)
 
-function GenerateOnlyUpperCase()
-{
-//Ie: A,B,C,D,E,F,G...Z
-let abc="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
-let result = abc.toUpperCase()
-return result;
+
+function GenerateOnlyUpperCase() {
+    //Ie: A,B,C,D,E,F,G...Z
+    let result=[]
+    let abc = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+    let ABC = abc.toLowerCase()
+    let arrayABC = ABC.split(',')
+    // console.log(arrayABC.length)
+    for (let i = 0; i <= randomNum(1, 26); i++) {
+        result.push(arrayABC[randomNum(1, 26)])
+    }
+    return result;
 }
 console.log(GenerateOnlyUpperCase())
 
-function GenerateOnlyLowerCase()
-{
-//Ie: a,b,c,d,e,f,g...z
-let abc="A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"
-let result = abc.toLowerCase()
-return result;
+function GenerateOnlyLowerCase() {
+    //Ie: a,b,c,d,e,f,g...z
+    let result=[]
+    let ABC = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"
+    let abc = ABC.toUpperCase()
+    let arrayabc = abc.split(',')
+    // console.log(arrayABC.length)
+    for (let i = 0; i <= randomNum(1, 26); i++) {
+        result.push(arrayabc[randomNum(1, 26)])
+    }
+    return result;
 }
 console.log(GenerateOnlyLowerCase())
 
 
-function GenerateOnlyNumbers()
-{
-//Ie: 0,1,2,3...9
-let list = []
-for (i=0;i<=9;i++){
-    list.push(i)
-}
-return list;
+function GenerateOnlyNumbers() {
+    //Ie: 0,1,2,3...9
+    let list = []
+    for (i = 0; i <= 9; i++) {
+        list.push(i)
+    }
+    return list;
 }
 console.log(`Generate only number "0~9" = ${GenerateOnlyNumbers()}`)
 
@@ -78,37 +88,38 @@ function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function GenerateOnlySymbols()
-{
-//Ie: $, %, !, #, etc
-// 33-47
-// 91-96
-// 58-64
-// 123-126
-let symbol = ["+",",",".","-","'","\"","&","!","?",":",";","#","~","=","/","$","£","^","(",")","_","<",">"]
-let resultSymbol = []
-for (let i=0;i<=symbol.length;i++){
-    resultSymbol.push(symbol[i])
-}
-return resultSymbol;
+function GenerateOnlySymbols() {
+    //Ie: $, %, !, #, etc
+    // 33-47
+    // 91-96
+    // 58-64
+    // 123-126
+
+    let symbol = ["+", ",", ".", "-", "'", "\"", "&", "!", "?", ":", ";", "#", "~", "=", "/", "$", "£", "^", "(", ")", "_", "<", ">"]
+    console.log(symbol.length)
+    let resultSymbol = []
+    for (let i = 0; i <= randomNum(1, 23); i++) {
+        resultSymbol.push(symbol[randomNum(1, 23)])
+        NonDuplist = resultSymbol.join("")
+    }
+    return NonDuplist;
 }
 // let result = 
-console.log(GenerateOnlySymbols())
+console.log(`Generate only symbols = ${GenerateOnlySymbols()}`)
 
-function GenerateUpperAndLowerCase()
-{
+function GenerateUpperAndLowerCase() {
     //Ie: A,B,C,D,E,F,G...Z and  a,b,c,d,e,f,g...z
     let list = []
-    list.push(GenerateOnlyUpperCase()," & ",GenerateOnlyLowerCase())
+    list.push(GenerateOnlyUpperCase(), " & ", GenerateOnlyLowerCase())
     return list;
 }
 
 console.log(`Create upper case and lower case ${GenerateUpperAndLowerCase()}`)
 
 // }
-document.body.innerHTML = 
-(`
-concatanate a(HIHI) with b(How are you?) =  <b>${concatanate("HIHI, ","How are you?")}</b>
+document.body.innerHTML =
+    (`
+concatanate a(HIHI) with b(How are you?) =  <b>${concatanate("HIHI, ", "How are you?")}</b>
 <br><br>
 Take the number 65 and covert it to ASCII char =  <b>${ASCIINumberToChar(65)}</b>
 <br><br>
@@ -121,6 +132,8 @@ Text to upper case =  <b>${GenerateOnlyUpperCase()}</b>
 Text to lower case =  <b>${GenerateOnlyLowerCase()}</b>
 <br><br>
 Generate only number "0~9" = <b>${GenerateOnlyNumbers()}</b>
+<br><br>
+Generate only symbols : <b>${GenerateOnlySymbols()}</b>
 <br><br>
 Create upper case and lower case = <b>${GenerateUpperAndLowerCase()}</b>
 `)
