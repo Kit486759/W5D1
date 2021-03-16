37f706f615ce8e8ef9dbff03d5673d988d16e2d3
kit486759 <kit486759@gmail.com>
2021-03-16 00:39:08 -0700

    -a

--- numbers/numbers.js
+++ numbers/numbers.js
@@ -125,22 +125,55 @@ function RandomNumberListA(start, end) {
 console.log(`start from 2 ,end with 10 and random number between `, RandomNumberListA(2, 10));
 
 
-
-function RandomNumberList(start, end, count, allowDuplicates) {
-  // return count number of random numbers, between start and end. use allowDuplicates to choose if we should allow duplicates in the list or not
+function RandomNumberList(start, end, allowDuplicates) {
+  // return count number of random numbers, between start and end.
+  // use allowDuplicates to choose if we should allow duplicates in the list or not
 
   let list = []
   count = randomNum(start, end);
   for (let i = start; i <= count; i++) {
     
     if (allowDuplicates === true) {
-      list.push(randomNum(start, end))
+      list.push(i);
     } else {
       list.push(i);
+      NonDuplist = [...new Set(list)];
+
     }
   }
   return list;
 }
-console.log(NumberListC(3, 9, true))
+console.log(`From 1 to 10 , with random count and set allow duplicates :true`,RandomNumberList(1, 10, false))
+
+document.body.innerHTML = 
+(`
+
+Function ${a} + ${b} =  <b>${add(a, b)}</b>
+<br><br>
+Function ${a} - ${b} =  <b>${sub(a, b)}</b>
+<br><br>
+Function ${a} x ${b} =  <b>${mul(a, b)}</b>
+<br><br>
+Function ${a} / ${b} =  <b>${div(a, b)} </b>
+<br><br>
+Function base ${a} power ${b} = <b>${Exponentiation(a, b)}</b> 
+<br><br>
+increment ${a} by ${b} = <b>${Increment(a, b)}</b> 
+<br><br>
+decrement ${a} by ${b} = <b>${Decrement(a, b)}</b> 
+<br><br>
+start from 2 ,end with 5<b>${NumberListA(2, 5)}</b> 
+<br><br>
+start from 2 ,end with 10 and skip 5 <br><b>${NumberListB(2, 10, 5)}</b>
+<br><br>
+start from 3 end at 9, random: true<br><b>${NumberListC(3, 9, true)}</b>
+<br><br>
+<b>3 is odd</b> ${IsOdd(3)}
+<br><br>
+<b>6 is not even</b> ${IsEven(6)}
+<br><br>
+Start from 2 ,end with 10 and random number between<br><b>${RandomNumberListA(2, 10)}</b>
+<br><br>
+From 1 to 10 , with random count and set allow duplicates :false<br><b>${RandomNumberList(1, 10, false)}</b>`)
 
 
